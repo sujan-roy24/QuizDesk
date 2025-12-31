e34ee22d7afea598002c5751060478f5dcdc1c9e
sujan-roy24 <sjnry25@gmail.com>
2025-12-31 14:06:57 +0600

    refactor: modify IRepository for async and cancellation support

--- QuizDesk/QuizDesk.Domain/Interfaces/IRepository.cs
+++ QuizDesk/QuizDesk.Domain/Interfaces/IRepository.cs
@@ -2,10 +2,11 @@
 {
     public interface IRepository<T> where T : class
     {
-        Task<T?> GetByIdAsync(Guid id);
-        Task<IEnumerable<T>> GetAllAsync();
-        Task AddAsync(T entity);
-        void Update(T entity);
-        void Delete(T entity);
+        Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<IReadOnlyList<T>> GetAllAsync(CancellationToken cancellationToken = default);
+
+        Task AddAsync(T entity, CancellationToken cancellationToken = default);
+        Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+        Task DeleteAsync(T entity, CancellationToken cancellationToken = default);
     }
 }
