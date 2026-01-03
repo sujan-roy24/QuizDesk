9d535d96c959841bcac1fb0b1ee81f329a75f19d
sujan-roy24 <sjnry25@gmail.com>
2026-01-03 22:01:43 +0600

    feat: Add token service and password hasher interfaces

---
+++ QuizDesk/QuizDesk.Application/Abstractions/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using QuizDesk.Application.Contracts.Auth;
+using QuizDesk.Domain.Entities;
+
+namespace QuizDesk.Application.Abstractions
+{
+    public interface ITokenService
+    {
+        Task<TokenResponse> GenerateTokens(User user);
+        Task<TokenResponse> RefreshToken(string refreshToken);
+        Task<bool> RevokeToken(string refreshToken);
+        Task<bool> ValidateToken(string token);
+    }
+}
